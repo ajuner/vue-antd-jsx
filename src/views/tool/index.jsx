6560b0e33d747dc05b3d200a8bd13b9462d7851a
cj01 <106791576@qq.com>
2020-11-05 10:45:01 +0800

    feat: add emit slot

--- src/views/tool/index.jsx
+++ src/views/tool/index.jsx
@@ -1,12 +1,28 @@
 import { defineComponent, ref } from 'vue'
 
+import Custom from './component/custom'
+
 export default defineComponent({
+
     setup() {
         const message = ref('tool')
 
-        const renderPage = () => {
-            return <h1>{message.value}</h1>
+        const getProps = (val) => {
+            console.log(val)
         }
-        return () => <>{renderPage()}</>
+
+        const renderCustom = () => {
+            //slots
+            const slots = {
+                red: () => <p>red slot</p>,
+                green: () => <p>green slot：green</p>,
+                default: () => <p>default slot：default</p>
+            }
+            return <div>
+                <h1>slot，emit</h1>
+                <Custom v-model={message.value} v-slots={slots} onGetprops={getProps}></Custom>
+            </div>
+        }
+        return () => <>{renderCustom()}</>
     }
 })
