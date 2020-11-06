542f8c86885749ae7fa1e09075ba00d12a74af1a
cj01 <106791576@qq.com>
2020-11-06 15:52:58 +0800

    feat: add directives

--- src/views/dashboard/index.jsx
+++ src/views/dashboard/index.jsx
@@ -1,12 +1,57 @@
 import { defineComponent, ref } from 'vue'
 
+import './index.scss'
+
+const customDirective = {
+    mounted(el) {
+        el.style.transition = 'all .3s'
+        el.style.transform = 'scale(0.5)'
+        el.style.opacity = '0.4'
+        var io = new IntersectionObserver(([ entry ])=>{
+            if( entry.isIntersecting ) {
+                el.style.transform = 'scale(1)'
+                el.style.opacity = '1'
+            } else {
+                el.style.transform = 'scale(0.5)'
+                el.style.opacity = '0.4'
+            }
+        }, {
+            threshold: 0,
+            root: null,
+            rootMargin: '0px'
+        })
+        
+        io.observe(el)
+    }
+}
+
 export default defineComponent({
+
+    directives: { autoshow: customDirective},
+
     setup() {
+
         const message = ref('home')
 
         const renderPage = () => {
-            return <h1>{message.value}</h1>
+
+            return <div class="bg">
+                <div class="div-auto" vAutoshow={message.value}>{message.value}</div>
+                <div class="div-auto" vAutoshow={message.value}>{message.value}</div>
+                <div class="div-auto" vAutoshow={message.value}>{message.value}</div>
+                <div class="div-auto" vAutoshow={message.value}>{message.value}</div>
+                <div class="div-auto" vAutoshow={message.value}>{message.value}</div>
+                <div class="div-auto" vAutoshow={message.value}>{message.value}</div>
+                <div class="div-auto" vAutoshow={message.value}>{message.value}</div>
+                <div class="div-auto" vAutoshow={message.value}>{message.value}</div>
+                <div class="div-auto" vAutoshow={message.value}>{message.value}</div>
+                <div class="div-auto" vAutoshow={message.value}>{message.value}</div>
+                <div class="div-auto" vAutoshow={message.value}>{message.value}</div>
+                <div class="div-auto" vAutoshow={message.value}>{message.value}</div>
+            </div>
+
         }
+        
         return () => <>{renderPage()}</>
     }
 })
